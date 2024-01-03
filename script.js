77a15ca7d589e7f35ede380fa6a35df14744692e
pathlocator <63250404+newPerson-tech@users.noreply.github.com>
2024-01-03 21:56:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
 
-// document.body.onload = addElement();
-
-function addElement(tag, text, wrapIdEl) {
-    //create new div el-t
-    const newDiv = document.createElement(tag);
-    //adding content
-    newDiv.innerHTML = text;
-    //adding new div to DOM
-    const wrapIdEl = document.getElementById(wrapIdEl);
-    if (wrapIdEl) {
-        wrapIdEl.appendChild(newDiv);
-    }
-}
-
 
 /* 1) Display on the page numbers from 10 to 20 in line separated by comma*/
+const numbersArray = [];
 
-let taskOne = document.getElementById('task-one');
-//creating empty array to write numbers in
-let numbersArray = [];
 for (let i = 10; i <= 20; i++) {
     numbersArray.push(i);
 }
 
-//converting array into a string
 let numbersString = numbersArray.toString();
 
-addElement(div, numbersString, taskOne);
+document.write(`Task One solution: ${numbersString}`);
 
 
 /* 2) Display squares of numbers from 10 to 20*/
 
-//getting html element
-const taskTwo = document.getElementById('task-two');
-
-//creating empty array to write numbers, using Math.pow to square each number
 let squaresArray = [];
 for (let i = 10; i <= 20; i++) {
     squaresArray.push(Math.pow(i, 2));
@@ -42,12 +21,11 @@ for (let i = 10; i <= 20; i++) {
 
 let squaresString = squaresArray.toString();
 
-taskTwo.innerHTML = squaresString;
+document.write(`<br>`);
+
+document.write(`Task Two solution: ${squaresString}`);
 
 // 3) Display multiplication table for number 7
-
-const taskThree = document.getElementById('task-three');
-
 
 function multiplyNum() {
     for (let i = 1; i <= 7; i++) {
@@ -61,14 +39,10 @@ function multiplyNum() {
     return;
 }
 
-multiplyNum();
-
-taskThree.innerHTML = `Find multiplication table for number 7 below`;
+document.write(`Task Three solution: ${multiplyNum()}`);
 
 
 // 4) Count summ of all integers from 1 to 15
-
-
 function countIntegers() {
     let summ = 0;
     for (let i = 0; i <= 15; i++) {
@@ -79,14 +53,14 @@ function countIntegers() {
     return summ;
 }
 
-const summarisingResult = countIntegers();
-
-document.write(`The summ of all integers in a range from 1 to 15 is: ${summarisingResult}`);
-
 document.write(`<br>`);
 
-// 5) Count multiplication of all integers in a range from 15 to 35 
+const summarisingResult = countIntegers();
 
+document.write(`Task Four solution: ${summarisingResult}`);
+
+
+// 5) Count multiplication of all integers in a range from 15 to 35 
 function multiplyIntegers() {
     let result = 1;
     for (let i = 15; i <= 35; i++) {
@@ -94,14 +68,13 @@ function multiplyIntegers() {
     }
     return result;
 }
-
-const multiplicationResult = multiplyIntegers();
-document.write(`The result of multiplying of all integers in a range from 15 to 35 is: ${multiplicationResult}`);
-
 document.write(`<br>`);
 
-// 6) Find arithmetical mean of all integers from 1 to 100
+const multiplicationResult = multiplyIntegers();
+document.write(`Task Five solution: ${multiplicationResult}`);
 
+
+// 6) Find arithmetical mean of all integers from 1 to 100
 function findArithmeticalMean() {
     let summ = 0;
     let average = 1;
@@ -110,13 +83,12 @@ function findArithmeticalMean() {
     }
     return average;
 }
+document.write(`<br>`);
 
-const averageResult = findArithmeticalMean()
+const averageResult = findArithmeticalMean();
+document.write(`Task Six solution: ${averageResult}`);
 
-document.write(`The arithmetical average of integers from 1 to 100 is: ${averageResult}`);
-
-//Display numbers from 100 to 300 which are multiple if 3
-
+// 7) Display numbers from 100 to 300 which are multiple if 3
 function multipleOfThree() {
     let numbers = [];
     for (let i = 100; i <= 300; i++) {
@@ -131,11 +103,10 @@ const numsMultipleOfThree = multipleOfThree();
 
 document.write(`<br>`);
 
-document.write(`Numbers multiple of 3 are: ${numsMultipleOfThree}`);
+document.write(`Task Seven solution: ${numsMultipleOfThree}`);
 
 
-//Find all natural number divisors
-
+// 8) Find all natural number divisors
 function naturalNumDivisors(natNum) {
     let divisors = [];
     for (let i = 1; i <= natNum; i++) {
@@ -146,14 +117,13 @@ function naturalNumDivisors(natNum) {
     return divisors;
 }
 
-const numberDivisors = naturalNumDivisors(13);
+const numberDivisors = naturalNumDivisors(10);
 
 document.write(`<br>`);
 
-document.write(`Divisors of a given number are: ${numberDivisors}`);
+document.write(`Task Eight solution: ${numberDivisors}`);
 
-// 10) Find all paired divisors of a number
-
+// 9) Find all paired divisors of a number
 function pairedNumDivisors(number) {
     const pairedDivisors = [];
     for (let i = 1; i <= Math.sqrt(number); i++) {
@@ -168,11 +138,10 @@ const divisorsPaired = pairedNumDivisors(10);
 
 document.write(`<br>`);
 
-document.write(`Paired divisors of a given number are: ${divisorsPaired}`);
-
-// 11) Count summ of all paired divisors of a number 
+document.write(`Task Nine solution: ${divisorsPaired}`);
 
 
+// 10) Count summ of all paired divisors of a number 
 function pairedDivisorsSumm(number) {
     const pairedDivisors = [];
     let pairedDivisorsSumm = 1;
@@ -189,10 +158,10 @@ const divisorsSumm = pairedDivisorsSumm(53);
 
 document.write(`<br>`);
 
-document.write(`Summ of paired divisors of a given number are: ${divisorsSumm}`);
+document.write(`Task Ten solution: ${divisorsSumm}`);
 
-// 12) Display multiplication table for all numbers
 
+// 11) Display multiplication table for all numbers
 function displayMultiplication() {
     for (let i = 1; i <= 10; i++) {
         for (let j = 1; j <= 10; j++) {
@@ -210,11 +179,3 @@ document.write(`Multiplication table:`)
 document.write(`<br>`);
 
 displayMultiplication();
-
-
-
-
-
-
-
-
